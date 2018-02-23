b82608ee602c5058a19786e3830f5bb455b788ed
Chetanpreet Malhi <Chetan@ChetanpreetsMBP.fios-router.home>
2018-02-22 21:26:49 -0500

    added a function i created and tested in a repl

--- src/App.js
+++ src/App.js
@@ -30,10 +30,43 @@ class App extends Component {
     this.fetch();
   }
 
+  // created this function in a repl 
+  main() {
+  //create array of promises for promise all to be able to calculate everything at once
+  var promises = [];
+  antData.data.ants.forEach((ant) => { 
+    const pinkyPromise = new Promise((resolve, reject) => {
+      generateAntWinLikelihoodCalculator()(resolve);
+    })
+      .then((data)=>{
+        var obj = {
+          likelihood: data,
+          ant: ant
+        }
+        console.log('Calculated likelihood of', obj.ant.name, 'winning')
+        return obj
+      })
+    promises.push(pinkyPromise);
+    console.log('ant order', ant.name)
+  });
+  
+  Promise.all(promises)
+    .then((values) => {
+      values = values.sort((a,b)=>{
+        return a.likelihood < b.likelihood
+      })
+      console.log('all calculations done \n', values)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+
   render() {
     return (
       <div className="App">
-        <h1>Competitive Ant Racing</h1>
+        <h1>- Extreme Ant Racing -</h1>
         <ButtonBar />
         <AntList ants={this.state.ants} />
       </div>
